9576c37a4aa743e89ef87a4eea9b0bfc34e89937
Pierre Migeon <pi3rre.migeon@gmail.com>
2023-05-16 14:59:25 -0700

    finish day 1 exercise 2, references and pointers

--- cpp01/ex02/main.cpp
+++ cpp01/ex02/main.cpp
@@ -1,11 +1,24 @@
-
-#include "Zombie.hpp"
-
+#include <stdlib.h>
+#include <string>
+#include <iostream>
 
 int	main() {
 	std::string example ("HI THIS IS BRAIN");
-	std::string *stringPTR = example;
-	std::string &stringREF ???...
+	std::string *stringPTR = &example;
+	std::string &stringREF = example;
+
+	//Print the address of the string, the address of the pointer, and the address of the reference
+	std::cout << &example << "\n";
+	std::cout << &stringPTR << "\n"; 
+	std::cout << &stringREF << "\n";
+
+	//Print the value of string, value of pointer, and value of the reference
+	std::cout << example << "\n";
+	std::cout << *stringPTR << "\n"; 
+	std::cout << stringREF << "\n";
+	
+
+
 
 	return (0);
 }
