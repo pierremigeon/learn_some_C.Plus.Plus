d4c3d70cb466bd1e96fdb4821f61bfc088ec0471
Pierre Migeon <pi3rre.migeon@gmail.com>
2023-05-24 02:12:01 -0700

    cpp01 ex01 add llist-> announce() and delete functions, freeing allocated space

--- cpp01/ex01/main.cpp
+++ cpp01/ex01/main.cpp
@@ -1,15 +1,27 @@
 #include "Zombie.hpp"
 
+void	call_hoard(Zombie *first) {
+	while (first) {
+		first->announce();
+		first = first->next;
+	}
+}
+
+void	free_hoard(Zombie *first) {
+	while (first) {
+		delete first;
+		first = first->next;
+	}
+}
+
 int	main() {
 	Zombie *first;
 	int num = 0;
 
 	std::cin >> num;
-	
 	first = zombieHorde(num, "Pierre");
-	while (first) {
-		first->announce();
-		first = first->next;
-	}
+	call_hoard(first);
+	free_hoard(first);
+
 	return (0);
 }
